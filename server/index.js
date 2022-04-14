89ece8cccc00f40373b894caae8f25945cfa6840
Aaron Dollard <daronaollard1995@gmail.com>
2022-04-14 17:31:03 +0100

    deployment

--- server/index.js
+++ server/index.js
@@ -18,6 +18,12 @@ app.use(helmet());
 app.use(cors(corsConfig));
 app.use(express.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 //Route below are used for authentication
 app.use("/auth", authRouter);
 app.set("trust proxy", 1);
