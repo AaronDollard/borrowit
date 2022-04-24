60ae8a6c421092bf4ec078b40f9891f365c84758
Aaron Dollard <daronaollard1995@gmail.com>
2022-04-24 21:27:44 +0100

    added html for registration email

--- server/controllers/express/attemptSignUp.js
+++ server/controllers/express/attemptSignUp.js
@@ -26,14 +26,70 @@ const attemptRegister = async (req, res) => {
             from: "borrowit400@gmail.com",
             to: req.body.email,
             subject: "Borrowit Registration",
-            html: `<h3>Hi ${req.body.username},</h3>
-
-            <p>Thank you for registering an account with Borrowit.ie!</p>
-            <p>We hope you enjoy borrowing and lending items with other users and saving money and enviornemnt in the process. Please note, Borrowit has full authority over the any items information you upload. If the items you upload are deemed unacceptable or harmful to others, Borrowit reserves the right to delete or alter the information that you provide on the item/items in question. With that out of the way, we wish for you to enjoy our free service and have fun lending and borrowing with like minded people.</p>
-            
-            <p>Cheers,<br>
-            The Borrowit Team
-            </p>`
+            html: `<!DOCTYPE html>
+            <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+            <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width,initial-scale=1">
+              <meta name="x-apple-disable-message-reformatting">
+              <title></title>
+              <!--[if mso]>
+              <noscript>
+                <xml>
+                  <o:OfficeDocumentSettings>
+                    <o:PixelsPerInch>96</o:PixelsPerInch>
+                  </o:OfficeDocumentSettings>
+                </xml>
+              </noscript>
+              <![endif]-->
+              <style>
+                table, td, div, h1, p {font-family: Arial, sans-serif;}
+              </style>
+            </head>
+            <body style="margin:0;padding:0;">
+              <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
+                <tr>
+                  <td align="center" style="padding:0;">
+                    <table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
+                      <tr>
+                        <td align="center" style="padding:40px 0 30px 0;background:#ffffff;">
+                          <img src="https://s00152905-project1.s3.eu-west-1.amazonaws.com/logo.png" alt="" width="300" style="height:auto;display:block;" />
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding:36px 30px 42px 30px;">
+                          <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                            <tr>
+                              <td style="padding:0 0 36px 0;color:#153643;">
+                                <h1 style="font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif;">Welcome to Borrowit ${req.body.username}</h1>
+                                <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">We hope you enjoy borrowing and lending items with other users and saving money and the planet in the process.</p>
+                                <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">Please note however, Borrowit has full authority over the any items information you upload. If the items you upload are deemed unacceptable or harmful to others, Borrowit reserves the right to delete or alter the information that you provide on the item/items in question. With that out of the way, we wish for you to enjoy our free service and have fun lending and borrowing with like minded people.</p>
+                                <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">With that out of the way, we wish for you to enjoy our free service and have fun lending and borrowing with like minded people.</p>
+                                <p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="https://borrowit.netlify.app" style="color:#1b846d;text-decoration:underline;">Begin borrowing and lending!</a></p>
+                              </td>
+                            </tr>
+                          </table>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding:30px;background:#1b846d;">
+                          <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;">
+                            <tr>
+                              <td style="padding:0;width:50%;" align="left">
+                                <p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;">
+                                  &reg; Borrowit Team 2022<br/>
+                                </p>
+                              </td>
+                            </tr>
+                          </table>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </body>
+            </html>`
         };
 
 
